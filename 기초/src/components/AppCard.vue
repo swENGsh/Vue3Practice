8afa4a50936c36c5625f3b408345383549509b87
고승효 <shko0524@naver.com>
2022-07-27 14:59:01 +0900

    0727 Single File Component

--- 기초/src/components/AppCard.vue
+++ 기초/src/components/AppCard.vue
@@ -1,7 +1,11 @@
 <template>
 	<div class="card">
 		<div class="card-body">
-			<h5 class="card-title">Card title</h5>
+			<!-- {{ $style }}
+			{{ classes }} -->
+			<h5 class="card-title red">Card title</h5>
+			<!-- <p class="card-text" :class="$style.red"> -->
+			<!-- <p class="card-text" :class="classes.red"> -->
 			<p class="card-text">
 				Some quick example text to build on the card title and make up the bulk
 				of the card's content.
@@ -12,13 +16,50 @@
 </template>
 
 <script>
+import { ref } from 'vue';
+// import { useCssModule } from 'vue';
 console.log('AppCard module');
 export default {
 	setup() {
+		// 함수의 객체를 setup 함수로 가져오고 싶을 때.
+		// const style = useCssModule();
+		// console.log('style: ', style);
 		console.log('AppCard setup()');
-		return {};
+
+		const color = ref('red');
+		// css의 v-bind 기능을 이용하여 동적으로 값 변경 가능
+		color.value = 'yellow';
+		return { color };
 	},
 };
 </script>
 
-<style lang="scss" scoped></style>
+<!-- scoped 속성의 여부에 따라서 전역/지역 나뉨 -->
+<!-- red 속성을 전역에서 사용하고 싶으면 <style>만-->
+<style>
+/* 동적바인드 */
+.red {
+	color: v-bind(color);
+}
+</style>
+
+<!-- red 속성을 AppCard.vue 내부에서만 사용하고 싶으면 <style scoped> 써주면 된다. -->
+<!-- <style scoped>
+.red {
+	color: red !important;
+}
+</style> -->
+
+<!-- <style module> 속성은 내장객체 형식으로 표현된다. -->
+<!-- <style module>
+.red {
+	color: red !important;
+}
+</style> -->
+
+<!-- module 명을 지정해줄 수 있음. -->
+<!-- <style module="classes">
+.red {
+	color: red !important;
+}
+</style> -->
