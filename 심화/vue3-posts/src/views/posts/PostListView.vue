c8552f7a395abf189b924fa590b01793063462fb
고승효 <shko0524@naver.com>
2022-07-28 07:34:09 +0900

    0728 vue-router:post-board UI

---
+++ 심화/vue3-posts/src/views/posts/PostListView.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <h2>게시글 목록</h2>
+    <hr class="my-4" />
+    <div class="row g-3">
+      <div v-for="post in posts" :key="post.id" class="col-4">
+        <PostItem
+          :title="post.title"
+          :content="post.content"
+          :create-at="post.createAt"
+          @click="goPage(post.id)"
+        ></PostItem>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import { getPosts } from '@/api/posts.js';
+import PostItem from '@/components/posts/PostItem.vue';
+
+const router = useRouter();
+const posts = ref([]);
+
+const fetchPosts = () => {
+  posts.value = getPosts();
+};
+fetchPosts();
+const goPage = id => {
+  // router.push(`/posts/${id}`);
+  // query / hash 들어가면
+  // https://localhost:5173/posts/2?searchText=hello#world!
+  router.push({
+    name: 'PostDetail',
+    params: {
+      id,
+    },
+    // query: {
+    //   searchText: 'hello',
+    // },
+    // hash: '#world!',
+  });
+};
+</script>
+
+<style lang="scss" scoped></style>
