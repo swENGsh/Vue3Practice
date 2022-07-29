45dabcaf7d634f3d6818146428596e1604d09ebe
고승효 <shko0524@naver.com>
2022-07-29 16:24:40 +0900

    404 Not Found & Nested Router

---
+++ 심화/vue3-posts/src/views/nested/NestedView.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- 탭 전환할거임 -->
+  <div>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <!-- 라우팅을 위한 라우터 링크 설정 -->
+        <RouterLink
+          class="nav-link"
+          active-class="active"
+          :to="{ name: 'NestedOne', replace: true }"
+          >Nested One</RouterLink
+        >
+      </li>
+      <li class="nav-item">
+        <RouterLink
+          class="nav-link"
+          active-class="active"
+          :to="{ name: 'NestedTwo', replace: true }"
+          >Nested Two</RouterLink
+        >
+      </li>
+    </ul>
+    <hr class="my-4" />
+    <!-- 중첩라우터이용 -->
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
