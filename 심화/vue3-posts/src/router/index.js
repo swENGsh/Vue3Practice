7c625d9e3aa17ba6d70d19f4e15e0fd3e1430b79
고승효 <shko0524@naver.com>
2022-07-30 13:45:27 +0900

    History Mode: SPA, SSR, CSR

--- 심화/vue3-posts/src/router/index.js
+++ 심화/vue3-posts/src/router/index.js
@@ -1,4 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import {
+  createRouter,
+  // createWebHashHistory,
+  createWebHistory,
+} from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import PostCreateView from '@/views/posts/PostCreateView.vue';
@@ -81,7 +85,24 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory('/'),
+  /**
+   * vue3는 SPA 기반의 프레임워크 => 자바스크립트로 UI 실행 => 페이지 로딩 없이 빠르게 UI 보여줌(리로딩x)
+   * SSR(Server Side Rendering) vs CSR(Client Side Rendering)
+   * SSR : 리로딩 일어남 => 페이지 요청 시 서버에서 리소스 제공(UI 보여줄 HTML 태그를 서버에서 만들어서 내려줌)
+   * CSR : 자바스크립트 코드를 이용하여 HTML 생성해서 사용자에게 보여줌.
+   *
+   * History Mode vs Hash Mode
+   * => 둘의 차이 : URI에 #이 붙냐 안붙냐의 차이
+   * History Mode: 전체 경로를 포함해서 리소스 요청.
+   * 대부분의 서버는 루트로만 요청 시 index.html 파일을 돌려줌. => 경로에 index.html 파일이 없으면 404.
+   * => 서버에 따라 추가 설정 필요.
+   * Hash Mode: 해쉬 뒤의 URI는 보내지 않는다. 도메인만 가지고 요청.
+   * 서버의 요청을 루트로만 보내므로 서버 추가 설정 없이 바로 배포 후 사용 가능.
+   * => 대부분 해쉬는 홈페이지를 찾는것이 아니라 홈페이지 내에서 스크롤하기 힘든 부분을 찾아가기 위해 사용.
+   *    따라서 검색 엔진은 해쉬를 하나의 페이지로 인식하지 못하는 경우가 많음. 결국 최적화(SEO)에 나쁜 영향.
+   */
+  history: createWebHistory('/'), // 히스토리 모드로 동작
+  // history: createWebHashHistory(), // 해쉬 모드로 동작
   routes,
 });
 
